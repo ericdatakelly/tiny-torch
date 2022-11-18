ce9f143bef36c4d931692cf1a1888ee55bb840bc
Eric Kelly <ericdatakelly@gmail.com>
2022-11-17 17:59:10 -0700

    Get env vars with dotenv.

--- workflows/hera_test.py
+++ workflows/hera_test.py
@@ -1,14 +1,16 @@
 import os
 
 import hera
+from dotenv import load_dotenv
 from hera import DAG, Parameter, Task, Workflow
 
 from tiny_torch.evaluate import evaluate
 from tiny_torch.main import main
 from tiny_torch.visualize import view_examples
 
-hera.set_global_token = os.environ['ARGO_TOKEN']
-hera.set_global_host = os.environ['ARGO_HOST']
+load_dotenv()
+hera.set_global_token = os.getenv('ARGO_TOKEN')
+hera.set_global_host = os.getenv('ARGO_SERVER')
 
 with Workflow('pipeine') as wf:
     Task(
