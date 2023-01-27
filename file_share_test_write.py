0f8d8727a685cc42ab412a0faa89fbcdef6b53bc
Eric Kelly <ericdatakelly@gmail.com>
2023-01-26 17:22:56 -0700

    Use boto to write to s3.

--- file_share_test_write.py
+++ file_share_test_write.py
@@ -1,2 +1,7 @@
+import boto3
+
+s3 = boto3.resource('s3')
+
 with open('artifact_test/data_test.txt', 'w') as f:
     f.write('fake data')
+    s3.Bucket('tiny-torch').put_object(Key='data_test.txt', Body=f)
