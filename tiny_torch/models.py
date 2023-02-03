597ac7fc0b471c69d9bff4a778b7486266f51c8b
vfdev-5 <vfdev.5@gmail.com>
2023-02-04 00:57:30 +0100

    Multiple updates - refactored config handling, added two configs - added profiling with tb - logger reports lr and throughput

--- tiny_torch/models.py
+++ tiny_torch/models.py
@@ -1,3 +1,4 @@
+from torch import optim
 from torchvision import models
 
 
@@ -8,3 +9,12 @@ def setup_model(name):
         raise RuntimeError(f"Unknown model name {name}")
 
     return fn(num_classes=10)
+
+
+def setup_optim(name, params, lr):
+    if name in optim.__dict__:
+        fn = optim.__dict__[name]
+    else:
+        raise RuntimeError(f"Unknown optimizer name {name}")
+
+    return fn(params, lr=lr)
